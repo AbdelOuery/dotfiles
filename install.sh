8bf837f90add1b1299b1b2b83dcb4c794555b071
AbdelOuery <oueryemchiabdelbasset@gmail.com>
2019-02-12 20:57:51 +0100

    Initialize dotfiles repository

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create symbolic links
+########################
+
+# Fonts
+ln -sf $PWD/fonts/ ~/.fonts/fonts
+
+# Zsh Themes
+ln -sf $PWD/zsh-themes/* ~/.oh-my-zsh/custom/themes/
+
+# Konsole colorschemes
+ln -sf $PWD/colorschemes/*.colorscheme ~/.local/share/konsole/
+
+# Git config
+ln -sf $PWD/gitconfig ~/.gitconfig
