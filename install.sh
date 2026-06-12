3845448f58f8bf6648445eb60880ebe5850f98c0
Abdelbasset Oueryemchi <AbdelOuery@users.noreply.github.com>
2026-06-12 21:07:54 +0200

    Enhance install.sh for vim plugins and configs
    Added vim plugin setup and configuration file creation.

--- install.sh
+++ install.sh
@@ -2,13 +2,22 @@
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-# Populating git user inside git_config
-echo "Setting up git user file!"
-read -p "Enter your username [ (b) bypass this step ]: " git_user
+echo "Setting up vim plugins..."
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
+
+echo "Setting up configuration files..."
+mkdir -p $HOME/.config/ghostty
+mkdir -p $HOME/.config/niri/config.kdl
 
 # Scripts and rcs
 ln -s $SCRIPT_PATH/git_config $HOME/.gitconfig
 ln -s $SCRIPT_PATH/vim_config $HOME/.vimrc
+ln -s $SCRIPT_PATH/ghostty_config.ghostty $HOME/.config/ghostty/config.ghostty
+
+# Populating git user inside git_config
+echo "Setting up git user file..."
+read -p "Enter your username [ (b) bypass this step ]: " git_user
 
 if [ $git_user != "b" ]; then
     read -p "Enter your git email: " git_email
